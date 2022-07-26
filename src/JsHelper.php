8a94e726a0522f18b0734039fba91adb4a8b6d59
ALGSUPPORT OÜ <8190132+algsupport@users.noreply.github.com>
2022-07-26 03:06:27 +0300

    Create JsHelper.php

---
+++ src/JsHelper.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace algsupport/yii2swiperjs8;
+
+use Yii;
+
+/**
+ * Js helper class for php.
+ */
+class JsHelper
+{
+    /**
+     * initVar function.
+     *
+     * @param string        $name
+     * @param object|string $value . Object type: \yii\web\JsExpression
+     *
+     * @return string
+     */
+    public static function initVar($name, $value)
+    {
+        return "var {$name} = {$value}";
+    }
+
+    /**
+     * addString function.
+     *
+     * @param string $value
+     *
+     * @return string
+     */
+    public static function addString($value)
+    {
+        return "\"{$value}\"";
+    }
+
+    /**
+     * newJsObject function.
+     *
+     * @param string $instanceName
+     * @param array  $params
+     *
+     * @return string \yii\web\JsExpression. See https://www.yiiframework.com/doc/api/2.0/yii-web-jsexpression for more information about it.
+     */
+    public static function newJsObject($instanceName, $params = [])
+    {
+        $params = implode(',', $params);
+
+        return new \yii\web\JsExpression("new {$instanceName}({$params})");
+    }
+}
